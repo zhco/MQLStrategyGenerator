f0a6e7068e36f5cb37ee2fcaf9dc4cb56dec643b
zhco <luycheung@qq.com>
2026-06-24 23:10:41 +0800

    Add app/src/main/java/com/marvis/mql/ui/theme/Theme.kt

---
+++ app/src/main/java/com/marvis/mql/ui/theme/Theme.kt
@@ -0,0 +1,26 @@
+package com.marvis.mql.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF1A73E8)
+val PrimaryDark = Color(0xFF1557B0)
+val Secondary = Color(0xFF34A853)
+val Error = Color(0xFFEA4335)
+val Warning = Color(0xFFFBBC04)
+val Background = Color(0xFFF8F9FA)
+val Surface = Color(0xFFFFFFFF)
+val OnPrimary = Color(0xFFFFFFFF)
+val OnBackground = Color(0xFF202124)
+val OnSurface = Color(0xFF3C4043)
+val CardBorder = Color(0xFFDADCE0)
+val DividerColor = Color(0xFFE8EAED)
+
+val BuyColor = Color(0xFFEF5350)
+val SellColor = Color(0xFF26A69A)
+val CodeBg = Color(0xFF1E1E1E)
+val CodeText = Color(0xFFD4D4D4)
+val KeywordColor = Color(0xFF569CD6)
+val FunctionColor = Color(0xFFDCDCAA)
+val NumberColor = Color(0xFFB5CEA8)
+val CommentColor = Color(0xFF6A9955)
+val StringColor = Color(0xFFCE9178)
